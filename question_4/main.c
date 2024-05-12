de839d92eeb351e0ed4ed24dc329e9e1e1a2a783
manbr21 <manbr@cin.ufpe.br>
2024-05-12 12:17:13 -0300

    código comentado do sudoku

--- question_4/main.c
+++ question_4/main.c
@@ -1,15 +1,18 @@
 /*
-5 3 4 | 6 7 8 | 9 1 2
-6 7 2 | 1 9 5 | 3 4 8
-1 9 8 | 3 4 2 | 5 6 7
+* * * | * * * | * * *
+* 1 * | * 2 * | * 3 *
+* * * | * * * | * * *
 ---------------------
-8 5 9 | 7 6 1 | 4 2 3
-4 2 6 | 8 5 3 | 7 9 1
-7 1 3 | 9 2 4 | 8 5 6
+* * * | * * * | * * *
+* 4 * | * 5 * | * 6 *
+* * * | * * * | * * *
 ---------------------
-9 6 1 | 5 3 7 | 2 8 4
-2 8 7 | 4 1 9 | 6 3 5
-3 4 5 | 2 8 6 | 1 7 9
+* * * | * * * | * * *
+* 7 * | * 8 * | * 9 *
+* * * | * * * | * * *
+
+A resolução foi feita pensando em atribuir o papel de cada thread checar se seu quadrado é válido.
+Cada número no centro representa o id da thread que cuida desse quadrado.
 */
 
 #include <stdio.h>
@@ -19,102 +22,106 @@
 
 #define NUM_THREADS 9 
 
-int tabuleiro[NUM_THREADS][NUM_THREADS];
+int tabuleiro[NUM_THREADS][NUM_THREADS]; //Criando o tabuleiro
 pthread_mutex_t mutex;
 
-int cont = 0;
+int cont = 0; //contador que checa a quantidade de números que estão inseridos de forma correta
 
 int valida(int linha, int coluna, int posi, int posj){
-    int erro = 0;
+    int erro = 0; //variável de controle pra checar quais condições são falsas
+
+    //para todos os casos abaixo não é considerado o pŕoprio elemento
     for(int i = 0; i < NUM_THREADS; i++){
-        if(i != posi && tabuleiro[posi][posj] == tabuleiro[i][posj]){
+        if(i != posi && tabuleiro[posi][posj] == tabuleiro[i][posj]){ //se houver elemento igual na mesma linha
             erro++;
         }
     }
 
     for(int j = 0; j < NUM_THREADS; j++){
-        if(j != posj && tabuleiro[posi][posj] == tabuleiro[posi][j]){
+        if(j != posj && tabuleiro[posi][posj] == tabuleiro[posi][j]){ //se houver elemento igual na mesma coluna
             erro++;
         }
     }
 
     for(int i = (linha - 1) * 3; i < 3 * linha; i++){
         for(int j = (coluna - 1) * 3; j < 3 * coluna; j++){
-            if(i != posi && j != posj && tabuleiro[posi][posj] == tabuleiro[i][j]){
+            if(i != posi && j != posj && tabuleiro[posi][posj] == tabuleiro[i][j]){ //se houver elemento igual no mesmo quadrado
                 erro++;
             }
         }
     }
-    // printf("%d\n", erro);
-    if(erro > 0){
+    
+    if(erro > 0){ //se erro não for zero, há pelo menos um motivo pra o número não estar lá
         return 0;
     }
     else{
         return 1;
     }
-
-    return 0;
 }
 
-void *checa(void *arg){
+void *checa(void *arg){ //essa função percorre cada um dos números do quadrado da respectiva thread
     int indice = *((int *) arg);
     int linha, coluna = (indice % 3) + 1;
 
-    if(indice <= 3){
+    if(indice <= 3){ //threads 1-3 cuidam da "primeira" linha de quadrados
         linha = 1;
     }
-    else if(indice > 3  && indice <= 6){
+    else if(indice > 3  && indice <= 6){ //threads 4-6 cuidam da "segunda linha de quadrados"
         linha = 2;
     }
-    else{
+    else{ //o resto das threads cuidam da última linha de quadrados
         linha = 3;
     }
     
-    //printf("LINHA %d DE QUADRADOS:\n", linha);
     for(int i = (linha - 1) * 3; i < 3 * linha; i++){
         for(int j = (coluna - 1) * 3; j < 3 * coluna; j++){
-            pthread_mutex_lock(&mutex);
-            cont += valida(linha, coluna , i, j);
-            pthread_mutex_unlock(&mutex);
+            if(valida(linha, coluna , i, j) == 1){ //se o número checado estiver na posição certa...
+                pthread_mutex_lock(&mutex); //travamos o mutex, pois as threads irão acessar a mesma zona crítica (global cont)
+                cont++;
+                pthread_mutex_unlock(&mutex); //destrava o mutex
+            }
         }
     }
 }
 
 int main(void){
     
-    int *thread_id[NUM_THREADS];
     pthread_mutex_init(&mutex, NULL);
 
-    for(int i = 0; i < NUM_THREADS; i++){
+    for(int i = 0; i < NUM_THREADS; i++){ //recebendo a entrada
         for(int j = 0; j < NUM_THREADS; j++){
             scanf("%d", &tabuleiro[i][j]);
         }
     }
 
-    // 1 pra cada quadrado
-    pthread_t threads[NUM_THREADS];
+    pthread_t threads[NUM_THREADS]; //cada thread cuida de um quadrado 3x3 do sudokus
+    int *thread_id[NUM_THREADS]; //vetor de id de threads pra ajudar na identificação dos quadrados
+
     for(int i = 0; i < NUM_THREADS; i++){
-        thread_id[i] = (int *) malloc(sizeof(int));
+        thread_id[i] = (int *) malloc(sizeof(int)); //alocando o ponteiro de int pra facilitar a conversão
         *(thread_id[i]) = i + 1;
-        int rc = pthread_create(&(threads[i]), NULL, checa, (void *) thread_id[i]);
+
+        int rc = pthread_create(&(threads[i]), NULL, checa, (void *) thread_id[i]); //criando threads
         if(rc){
             printf("erro na criacao de threads\n");
             exit(-1);
         }
     }
 
-    for(int i = 0; i < NUM_THREADS; i++){
+    for(int i = 0; i < NUM_THREADS; i++){ //dando join
         pthread_join(threads[i], NULL);
     }
 
-    if(cont == 81){
+    if(cont == NUM_THREADS * NUM_THREADS){ //se a quantidade de números certos for igual ao total de números, é válidos
         printf("O sudoku é válido\n");
     }
     else{
         printf("O sudoku é inválido\n");
     }
 
+    //destruindo mutex e dando exit
     pthread_exit(NULL);
     pthread_mutex_destroy(&mutex);
+
     return 0;
 }
