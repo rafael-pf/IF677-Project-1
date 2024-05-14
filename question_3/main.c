36ba6306e941e8534cec9950a739addffc955344
Rafael <rpf@cin.ufpe.br>
2024-05-14 16:34:44 -0300

    esqueleto da questao 3 feito

--- question_3/main.c
+++ question_3/main.c
@@ -1,4 +1,5 @@
 /*
+
     3. Implemente um simulador de operações bancárias usando threads.
     O programa deve ter N threads representando clientes e uma thread
     representando o banco. Os clientes devem realizar operações como
@@ -11,23 +12,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define NUM_TRHEADS 4  // 3 clientes + banco
+#define NUM_THREADS 4  // 3 clientes + banco
 
 typedef struct {
     int id;
     int saldo;
-} Conta;
+} Conta; // cada conta possui indentificador e saldo
 
-char arquivos[NUM_TRHEADS - 1][20] = { {"cliente1.txt"}, {"cliente2.txt"}, {"cliente3.txt"} };
+typedef struct {
+    Conta conta;
+    char* arquivo;
+} InputAcesso; // sera passado como argumento para a funcao, contem a conta do usuario e o arquivo com as operações a serem realizadas por ele
 
+char arquivos[NUM_THREADS - 1][20] = { {"cliente1.txt"}, {"cliente2.txt"}, {"cliente3.txt"} }; // nome dos arquivos (qtd de arquivos = qtd de acessos simultaneos)
+
+// operações disponiveis no banco
 int SACAR() {}
 int DEPOSITAR() {}
 int CONSULTAR() {}
 
-void* ACESSO_AO_BANCO() {}
+// funcao das thread de clientes
+void* ACESSO_AO_BANCO(void* acesso) {}
+
+// funcao da thread do banco
+void* BANCO() {}
 
 int main() {
+    pthread_t threads[NUM_THREADS];
+    InputAcesso* acessos;
 
+    acessos = malloc((NUM_THREADS - 1) * sizeof(InputAcesso));
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (!i) {
+
+        }
+        else {
+            acessos[i].arquivo = arquivos[i - 1];
+            acessos[i].conta.id = i - 1;
+            acessos[i].conta.saldo = 0;
+
+            int rc = pthread_create(&(threads[i]), NULL, ACESSO_AO_BANCO, (void*)&(acessos[i])); //criando threads
+            if (rc) {
+                printf("falha na criacao das threads\n");
+                exit(-1);
+            }
+        }
+    }
 
     return 0;
 }
+
+
+/*
+    ideia: clientes fazem requisições para o banco,
+    as requisições entram numa queue,
+    banco usa queue para se guiar,
+    duvida: como usar mutex para isso
+*/
