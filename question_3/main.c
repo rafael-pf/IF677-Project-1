a4d132eab3787597b77e8215ffccff2d4d3ccbde
Rafael <rpf@cin.ufpe.br>
2024-05-15 13:34:37 -0300

    funcao de acesso ao banco iniciada

--- question_3/main.c
+++ question_3/main.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #define NUM_THREADS 4  // 3 clientes + banco
 
@@ -20,11 +21,18 @@ typedef struct {
 } Conta; // cada conta possui indentificador e saldo
 
 typedef struct {
+    int idConta;
+    char* pedido;
+    int valor;
+} Requisicao; // informacoes da requisicao atual a ser resolvida pelo banco
+
+typedef struct {
     Conta conta;
     char* arquivo;
 } InputAcesso; // sera passado como argumento para a funcao, contem a conta do usuario e o arquivo com as operações a serem realizadas por ele
 
 char arquivos[NUM_THREADS - 1][20] = { {"cliente1.txt"}, {"cliente2.txt"}, {"cliente3.txt"} }; // nome dos arquivos (qtd de arquivos = qtd de acessos simultaneos)
+
 
 // operações disponiveis no banco
 int SACAR() {}
@@ -32,7 +40,51 @@ int DEPOSITAR() {}
 int CONSULTAR() {}
 
 // funcao das thread de clientes
-void* ACESSO_AO_BANCO(void* acesso) {}
+void* ACESSO_AO_BANCO(void* acesso) {
+    InputAcesso* inputAcesso = (InputAcesso*)acesso;
+
+    // separando informacoes do acesso ao banco
+    char* nomeArquivo = inputAcesso->arquivo;
+    int idConta = inputAcesso->conta.id;
+    int saldoConta = inputAcesso->conta.saldo;
+
+    FILE* arq = NULL;
+
+    // leitura do arquivo
+    arq = fopen(nomeArquivo, "r");
+
+    if (arq == NULL) {
+        printf("Falha ao abrir o arquivo\n");
+        exit(1);
+    }
+
+    // processamento dos comandos do arquivo
+    char texto[50] = {};
+    char* comando = NULL;
+    int qtd = 0;
+
+    while (fscanf(arq, "%49[^,],\n", texto) == 1) {
+        // processando cada linha do arquivo (<pedido> <valor?>,)
+        comando = strtok(texto, " ");
+        switch (comando[0]) {
+        case 'C':
+            printf("Cliente %d pediu pra consultar\n", idConta);
+            break;
+        case 'D':
+            qtd = atoi(strtok(NULL, "\n"));
+            printf("Cliente %d pediu pra depositar %d\n", idConta, qtd);
+            break;
+        case 'S':
+            qtd = atoi(strtok(NULL, "\n"));
+            printf("Cliente %d pediu pra sacar %d\n", idConta, qtd);
+            break;
+        default:
+            break;
+        }
+    }
+
+    fclose(arq);
+}
 
 // funcao da thread do banco
 void* BANCO() {}
@@ -44,12 +96,13 @@ int main() {
     acessos = malloc((NUM_THREADS - 1) * sizeof(InputAcesso));
 
     for (int i = 0; i < NUM_THREADS; i++) {
+        // primeira thread é a do banco, as outras sao dos clientes
         if (!i) {
 
         }
         else {
             acessos[i].arquivo = arquivos[i - 1];
-            acessos[i].conta.id = i - 1;
+            acessos[i].conta.id = i;
             acessos[i].conta.saldo = 0;
 
             int rc = pthread_create(&(threads[i]), NULL, ACESSO_AO_BANCO, (void*)&(acessos[i])); //criando threads
@@ -59,6 +112,17 @@ int main() {
             }
         }
     }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (!i) {
+
+        }
+        else {
+            pthread_join(threads[i], NULL);
+        }
+    }
+
+    pthread_exit(NULL);
 
     return 0;
 }
