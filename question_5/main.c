c824f2c8935124f0366bc5f00214d9c562cb31e2
manbr <manbr@g5c46.cin.ufpe.br>
2024-05-15 15:22:51 -0300

    código funcional, falta adaptações

--- question_5/main.c
+++ question_5/main.c
@@ -6,44 +6,59 @@
 #include <stdlib.h>
 
 #define NUM_THREADS 2
-#define LIN 2
-#define COL 2
+#define LIN 2 //(numero de equaçoes)
+#define COL 2 //(numero de variaveis)
 
 pthread_barrier_t barreira;
 pthread_mutex_t mutex;
 
-int coef[LIN][COL] = {{2,1}, {5, 7}};
-int res[LIN] = {11, 13};
-float var[LIN] = {1, 1};
+int coef[LIN][COL] = {{2,1}, {5, 7}}; //matriz de coeficicientes do sistema
+int res[LIN] = {11, 13}; //vetor de resultados do sistema
+float var[LIN] = {1, 1}; //vetor de variáveis
 
-int p = 2;
+/*
+    O sistema usado de exemplo seria:
+    {2x1 + x2 = 11
+    {5x1 + 7x2 = 13
+*/
 
-void *cria(void *arg){
-    int id = *((int *) arg);
+int p = 5; //controla a aproximação do algoritmo
 
-    int x = 1, k = 0;
+void *cria(void *arg){ //função que calcula as variáveis
+
+    int id = *((int *) arg); //pegando id
+
+    int k = 0; //k da questao
     
     while(k < p){
-        int sum = 0;
+
+        //calculo do somatório
+        float sum = 0; 
         for(int j = 0; j < NUM_THREADS; j++){
             if(id != j){
                 sum += coef[id][j] * var[j];
             }
+            pthread_barrier_wait(&barreira); //esperando as threads sincronizarem para impedir que uma passe da outra no loop
         }
+
+        //lidando com a região crítica (variável global)
         pthread_mutex_lock(&mutex);
-        printf("id:%d sum:%d\n", id,sum);
         var[id] = (1.0 / coef[id][id]) * (res[id] - sum);
-        printf("%f\n", var[id]);
         pthread_mutex_unlock(&mutex);
-
-        pthread_barrier_wait(&barreira);
         k++;
+        
+        //esperando as threads calcularem xi 
+        pthread_barrier_wait(&barreira); // ÈH O BRUNAOO
     }
-
 }
 
 int main(void){
     
+    if(LIN < COL){
+        printf("o sistema é indeterminado\n");
+        exit(1);
+    }
+    //inicialização dos recursos
     pthread_barrier_init(&barreira, NULL, NUM_THREADS);
     pthread_mutex_init(&mutex, NULL);
 
@@ -53,7 +68,7 @@ int main(void){
     for(int i = 0; i < NUM_THREADS; i++){
         taskID[i] = (int *) malloc(sizeof(int));
         *taskID[i] = i;
-        int rc = pthread_create(&threads[i], NULL, cria, (void*) taskID[i]);
+        int rc = pthread_create(&(threads[i]), NULL, cria, (void*) taskID[i]);
         if(rc){
             printf("erro na criacao de threads\n");
             exit(-1);
@@ -68,17 +83,18 @@ int main(void){
         }
     }
 
+    //printando a solução do sistema
     for(int i = 0; i < NUM_THREADS; i++){
         printf("x%d = %f\n", i + 1, var[i]);
     }
 
-    pthread_exit(NULL);
-    pthread_barrier_destroy(&barreira);
-    pthread_mutex_destroy(&mutex);
-
+    //destruindo os recursos
     for(int i = 0; i < NUM_THREADS; i++){
         free(taskID[i]);
     }
-
+    pthread_exit(NULL);
+    pthread_barrier_destroy(&barreira);
+    pthread_mutex_destroy(&mutex);
+    
     return 0;
 }
