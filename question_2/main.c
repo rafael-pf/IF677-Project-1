f4b0fc891719218c33ecfef628064c947fa923f8
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2024-05-14 22:07:57 -0300

    update question_2

--- question_2/main.c
+++ question_2/main.c
@@ -3,13 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define NUM_THREADS 3
+#define NUM_THREADS 5
 
 pthread_barrier_t barrier;
 
 typedef struct conglomerado{
     int *array;
-    unsigned int start;
     unsigned int size;
 }Conglomerado;
 
@@ -19,12 +18,12 @@ void swap(int *array, int i, int j){
     array[j] = tmp;
 }
 
-void *bubblesort(void *agregados){//bubblesort tradicional
-    Conglomerado *ptr = (Conglomerado *)agregados;
-    unsigned int start = ptr->start, size = ptr->size;
+void *bubblesort(void *arg){//bubblesort tradicional
+    Conglomerado *ptr = (Conglomerado *)arg;
+    unsigned int size = ptr->size;
     int *array = ptr->array;
-    for(int i=start;i<size-1;i++){
-        for(int j=start;j<size-1-i;j++){
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-1-i;j++){
             //printf("%d > %d?\n", j, j+1);
             if(array[j] > array[j+1]){
                 swap(array, j, j+1);
@@ -32,6 +31,22 @@ void *bubblesort(void *agregados){//bubblesort tradicional
         }
     }
     pthread_barrier_wait(&barrier);
+}
+
+void *merge(void *arg){
+    Conglomerado *ptr = (Conglomerado *) arg;
+    int *array = ptr->array;
+    unsigned int size = ptr->size;
+    for(int i=0;i<size;i++){
+        int v = array[i];
+        int j = i-1;
+        while(j>=0 && array[j]>v){
+            array[j+1] = array[j];
+            j = j-1;
+        }
+        array[j+1] = v;
+    }
+
 }
 
 int main() {
@@ -47,10 +62,10 @@ int main() {
     pthread_barrier_init(&barrier, NULL, NUM_THREADS);
 
     unsigned int start=size, passo = size/NUM_THREADS;
-    for(int i=0, j=0;i<size && j<NUM_THREADS;i+= passo, j++){
-        printf("Criando Thread %d, size:%d start:%d\n", j, size, start);
-        pthread_create(&threads[j], NULL, bubblesort, ((void *) &(Conglomerado){.array=vet, .size=size+start, .start=start}));
-        start -= passo;
+    for(int i=0, thread_index=0; i<size && thread_index<NUM_THREADS; i+= passo, thread_index++){
+        printf("Criando Thread %d, passo:%d i:%d\n", thread_index, passo, i);
+        pthread_create(&threads[thread_index], NULL, bubblesort, ((void *) &(Conglomerado){.array=(vet+i), .size=passo}));
+        
     }
     
     for(int i=0; i<NUM_THREADS;i++){
@@ -61,10 +76,20 @@ int main() {
         }
     }
 
+    pthread_t thread_merge;
+    pthread_create(&thread_merge, NULL, merge, ((void *) &(Conglomerado){.array=vet, .size=size}));
+    int rc = pthread_join(thread_merge, NULL);
+    if(rc){
+            fprintf(stderr, "Erro na criacao do thread\n");
+            exit(-1);
+        }
+
     for(int i=0;i<size;i++){
         printf("%d ", vet[i]);
     }
     printf("\n");
+
+
 
     pthread_barrier_destroy(&barrier);
     pthread_exit(NULL);
