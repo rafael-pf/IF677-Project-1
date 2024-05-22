fdf46d3f77b8ee3c2f31dc5342c64f3193cb93f1
joaopbmarins <136388066+joaopbmarins@users.noreply.github.com>
2024-05-21 22:46:41 -0300

    add question_6

---
+++ question_6/main.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <time.h>
+#include "queue.h"
+
+#define NUM_THREADS 4
+#define NUM_OPERATIONS 100
+
+int a = 3;
+int b = 2;
+
+pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+int nucleos_trabalhando = 0;
+
+void segura() {
+    for (int i = 0; i < 10000000; i++) {}
+    if (nucleos_trabalhando > 4) printf("\tDeu merda\n");
+}
+
+void* somar(void* arg) {
+    printf("soma: %d\n", a + b);
+
+    segura();
+
+    pthread_mutex_lock(&mymutex);
+    nucleos_trabalhando--;
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mymutex);
+
+    pthread_exit(NULL);
+}
+
+void* subtrair(void* arg) {
+    printf("subtracao: %d\n", a - b);
+
+    segura();
+
+    pthread_mutex_lock(&mymutex);
+    nucleos_trabalhando--;
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mymutex);
+
+    pthread_exit(NULL);
+}
+
+void* multiplicar(void* arg) {
+    printf("produto: %d\n", a * b);
+
+    segura();
+
+    pthread_mutex_lock(&mymutex);
+    nucleos_trabalhando--;
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mymutex);
+
+    pthread_exit(NULL);
+}
+
+void* dividir(void* arg) {
+    printf("quociente: %d\n", a / b);
+
+    segura();
+
+    pthread_mutex_lock(&mymutex);
+    nucleos_trabalhando--;
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mymutex);
+
+    pthread_exit(NULL);
+}
+
+void carregaFuncoes(Funcao* array, int size) {
+
+    strcpy(array[0].nome, "Somar");
+    array[0].ptr = somar;
+    strcpy(array[1].nome, "Subtrair");
+    array[1].ptr = subtrair;
+    strcpy(array[2].nome, "Multiplicar");
+    array[2].ptr = multiplicar;
+    strcpy(array[3].nome, "Dividir");
+    array[3].ptr = dividir;
+
+}
+
+void carregaFila(Funcao* array, int size, Queue* queue) {
+    srand(time(NULL));
+    int index = rand() % size;
+
+    for (int i = 0; i < NUM_OPERATIONS; i++) {
+        enqueue(queue, array[index]);
+        index = rand() % size;
+    }
+}
+
+void* escalonador(void* arg) {
+
+    Queue* lista_pronto = (Queue*)arg;
+    int size = lista_pronto->size;
+    pthread_t threads[size];
+    int i = 0;
+
+    while (lista_pronto->size) {
+        Funcao f = lista_pronto->front->next->funcao;
+        dequeue(lista_pronto);
+
+        // cria thread
+        int rc = pthread_create(&(threads[i]), NULL, f.ptr, NULL);
+        if (rc) {
+            printf("erro na execucao de threads\n");
+            exit(-1);
+        }
+        i++;
+
+        pthread_mutex_lock(&mymutex);
+        nucleos_trabalhando++;
+        while (nucleos_trabalhando == NUM_THREADS) {
+            printf("Tudo ocupado!!!!\n");
+            pthread_cond_wait(&full, &mymutex);
+        }
+        pthread_mutex_unlock(&mymutex);
+    }
+
+    pthread_exit(NULL);
+}
+
+int main() {
+
+    Queue* queue = create_queue();
+    pthread_t thread_escalonador;
+    unsigned int size = 4;
+    Funcao funcoes[size];
+
+    carregaFuncoes(funcoes, size);
+
+    carregaFila(funcoes, size, queue);
+
+    int rc = pthread_create(&thread_escalonador, NULL, escalonador, (void*)queue);
+    if (rc) {
+        printf("erro na execucao de threads\n");
+        exit(-1);
+    }
+
+    pthread_join(thread_escalonador, NULL);
+
+    pthread_mutex_destroy(&mymutex);
+    pthread_cond_destroy(&full);
+    pthread_exit(NULL);
+
+    return 0;
+}
---
+++ question_6/queue.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+Node* create_empty_node(Node* next) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->next = next;
+    return n;
+}
+
+Node* create_node(Funcao req, Node* next) {
+    Node* n = (Node*)malloc(sizeof(Node));
+    n->funcao = req;
+    n->next = next;
+    return n;
+}
+
+Queue* create_queue() {
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = q->rear = create_empty_node(NULL);
+    q->size = 0;
+    return q;
+}
+
+void enqueue(Queue* q, Funcao req) {
+    q->rear->next = create_node(req, NULL);
+    q->rear = q->rear->next;
+    q->size++;
+}
+
+void dequeue(Queue* q) {
+    if (q->size != 0) {
+        Node* tmp = q->front->next;
+        q->front->next = q->front->next->next;
+        if (q->front->next == NULL) {
+            q->rear = q->front;
+        }
+        free(tmp);
+        q->size--;
+    }
+}
+
+void clear(Queue* q) {
+    for (int i = 0; i < q->size; i++) {
+        dequeue(q);
+    }
+    free(q);
+}
